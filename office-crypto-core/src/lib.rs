c7b00ecfd28ea029497f22b32c05af08960a0ef4
Kenta Kudo <yd37574@gmail.com>
2024-09-26 16:29:59 +0900

    refactor(core): cleanup

--- office-crypto-core/src/lib.rs
+++ office-crypto-core/src/lib.rs
@@ -5,16 +5,3 @@ mod standard;
 mod utils;
 
 pub use crypto::decrypt_from_bytes;
-
-#[cfg(test)]
-mod test {
-    #[test]
-    fn test_docx() {
-        let docx = include_bytes!("test.docx").to_vec();
-        let expected = include_bytes!("test_decrypted.docx").to_vec();
-
-        let bytes = crate::crypto::decrypt_from_bytes(&docx, "test").unwrap();
-
-        assert_eq!(bytes, expected);
-    }
-}
